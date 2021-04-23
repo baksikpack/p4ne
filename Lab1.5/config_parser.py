eb4046bb779a1be0b3c0c1c76abe3575ace705fd
am.bakin <baksikpack@gmail.com>
2021-04-23 16:56:08 +0300

    Initial commit

---
+++ Lab1.5/config_parser.py
@@ -0,0 +1,13 @@
+import glob
+
+ip = set()
+
+for file_name in glob.glob('C:\\Users\\am.bakin\\Documents\\!ОБУЧЕНИЕ\\Python\\config_files\\*.txt'):
+    with open(file_name) as f:
+        for line in f:
+            if line.find("ip address") == 1:
+                ip.add(line.replace("ip address", "").strip())
+
+for i in ip:
+    print(i)
+
