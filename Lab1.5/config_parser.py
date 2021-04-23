47350f2336476b21154e13616907c61d5e55e6f8
am.bakin <baksikpack@gmail.com>
2021-04-23 16:59:09 +0300

    Initial commit

--- Lab1.5/config_parser.py
+++ Lab1.5/config_parser.py
@@ -5,8 +5,8 @@ ip = set()
 for file_name in glob.glob('C:\\Users\\am.bakin\\Documents\\!ОБУЧЕНИЕ\\Python\\config_files\\*.txt'):
     with open(file_name) as f:
         for line in f:
-            if line.find("ip address") == 1:
-                ip.add(line.replace("ip address", "").strip())
+            if line.find("ip address")==1:
+                ip.add(line.replace("ip address","").strip())
 
 for i in ip:
     print(i)
